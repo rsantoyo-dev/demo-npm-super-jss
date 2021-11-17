2b9fcee3c80fe6d8da3648dcc26651ad0be4bda8
ricardo <ricardo.santoyo@hotmail.com>
2021-11-16 20:22:54 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {SuperJssModule} from "super-jss";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SuperJssModule
   ],
   providers: [],
   bootstrap: [AppComponent]
